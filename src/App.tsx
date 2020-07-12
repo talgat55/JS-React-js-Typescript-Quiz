9cf7f1e9972a4bc64710575ead795d9434b2d25c
talgat55 <lightx_design@yahoo.com>
2020-07-13 02:40:05 +0600

    add tempalte questioncard

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import QuestionCard from "./components/QuestionCard";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const startTrivia = async() =>{
+
+    };
+
+    const checkAnswer = ( e: React.MouseEvent<HTMLButtonElement>) => {
+
+    };
+
+    const nextQuestion = () =>{
+
+    };
+
+
+    return (
+        <div className="App">
+            <h1> Quiz</h1>
+            <button
+                className="start"
+                onClick={startTrivia}
+            >
+                Start
+            </button>
+            <p
+                className="score"
+            >
+                Score
+            </p>
+            <p>Loading</p>
+            <QuestionCard/>
+            <button
+                className="next"
+                onClick={nextQuestion}
+            >
+                Next
+            </button>
+        </div>
+    );
+};
 
 export default App;
---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+type Props = {
+    question: string;
+    answers: string[];
+    callback: any;
+    userAnswer: boolean;
+    questionNr: number;
+    totalQuestion: number;
+};
+const QuestionCard: React.FC<Props> = (
+    {
+        question,
+        answers,
+        callback,
+        userAnswer,
+        questionNr,
+        totalQuestion
+    }
+) => (
+    <div>
+        <p className="number">
+            Question: {questionNr} / {totalQuestion}
+        </p>
+        <p dangerouslySetInnerHTML={{__html: question}}/>
+        <div>
+            {
+                answers.map(answer => (
+                    <div>
+                        <button
+                            disabled={userAnswer}
+                            onClick={callback}
+                        >
+                            <span dangerouslySetInnerHTML={{__html: answer}}/>
+                        </button>
+                    </div>
+                ))
+            }
+        </div>
+    </div>
+);
+
+export default QuestionCard;
